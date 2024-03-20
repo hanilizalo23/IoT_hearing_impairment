b072d4e04eea6a654fb8c8d598383dbdbfde160b
hanilizalo23 <nelida_phm@hotmail.com>
2024-03-20 13:37:14 -0600

    Testing both microphones

--- sensor_ky037/sensor_ky037.ino
+++ sensor_ky037/sensor_ky037.ino
@@ -1,33 +1,28 @@
-int digitalPin = 7;   // KY-037 digital interface
-int analogPin = A0;   // KY-037 analog interface
-int ledPin = 13;      // Arduino LED pin
-int digitalVal;       // digital readings
-int analogVal;        // analog readings
+int analogPin_1 = A0;  // KY-037 analog interface microphone 1
+int analogPin_2 = A4;  // KY-037 analog interface microphone 2
+int analogVal_1;       // Analog readings for microphone 1
+int analogVal_2;       // Analog readings for microphone 2
 
-void setup()
-{
-  pinMode(digitalPin,INPUT); 
-  pinMode(analogPin, INPUT);
-  pinMode(ledPin,OUTPUT);      
+void setup() {
+  pinMode(analogPin_1, INPUT);
+  pinMode(analogPin_2, INPUT);
   Serial.begin(300);
 }
 
-void loop()
-{
-  // Read the digital inteface
-  digitalVal = digitalRead(digitalPin); 
-  
-  if(digitalVal == HIGH) 
-  {
-    digitalWrite(ledPin, HIGH); // Turn ON Arduino's LED
-  }
-  else
-  {
-    digitalWrite(ledPin, LOW);  // Turn OFF Arduino's LED
-  }
+void loop() {
+  // Read analog interface from microphones
+  analogVal_1 = analogRead(analogPin_1);
+  analogVal_2 = analogRead(analogPin_2);
 
-  // Read analog interface
-  analogVal = analogRead(analogPin);
-  // Print analog value to serial
-  Serial.println(analogVal);  
+  // Print analog value to serial of microphone 1
+  Serial.print("Microphone 1: ");
+  Serial.println(analogVal_1);
+  if (analogVal_1 >= 40)
+  Serial.println("Device is ready - Washing Machine");
+
+  // Print analog value to serial of microphone 2
+  Serial.print("Microphone 2: ");
+  Serial.println(analogVal_2);
+  if (analogVal_2 >= 45)
+    Serial.println("Device is ready - Microwave Oven");
 }
