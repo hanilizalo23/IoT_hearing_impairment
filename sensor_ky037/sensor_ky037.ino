f0ffa9083a9aabd28628d0b7898bec997c3ad303
hanilizalo23 <nelida_phm@hotmail.com>
2024-04-29 11:20:23 -0600

    Threshold settled

--- sensor_ky037/sensor_ky037.ino
+++ sensor_ky037/sensor_ky037.ino
@@ -1,5 +1,5 @@
-int analogPin_1 = 2;   // KY-037 analog interface microphone 1
-int analogPin_2 = 4;   // KY-037 analog interface microphone 2
+int analogPin_1 = 34;  // KY-037 analog interface microphone 1
+int analogPin_2 = 35;  // KY-037 analog interface microphone 2
 int analogVal_1;       // Analog readings for microphone 1
 int analogVal_2;       // Analog readings for microphone 2
 
@@ -10,19 +10,19 @@ void setup() {
 }
 
 void loop() {
-  // Read analog interface from microphones
+  //Read analog interface from microphones
   analogVal_1 = analogRead(analogPin_1);
   analogVal_2 = analogRead(analogPin_2);
 
-  // Print analog value to serial of microphone 1
+  //Print analog value to serial of microphone 1
   Serial.print("Microphone 1: ");
   Serial.println(analogVal_1);
-  if (analogVal_1 >= 65)
+  if (analogVal_1 >= 95)
   Serial.println("Device is ready - Washing Machine");
 
-  // Print analog value to serial of microphone 2
+  //Print analog value to serial of microphone 2
   Serial.print("Microphone 2: ");
   Serial.println(analogVal_2);
-  if (analogVal_2 >= 135)
-    Serial.println("Device is ready - Microwave Oven");
+  if (analogVal_2 >= 110)
+  Serial.println("Device is ready - Microwave Oven");
 }
